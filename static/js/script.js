8150677be8239b7fa06dbdc3a7c397d86c82fe15
Jeet Khondker <jeetzhkhondker@gmail.com>
2021-01-31 08:03:32 +0900

    feature: Created Function for calculating age in days

---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+// Challenge 1 : Age In Days
+function getAgeInDays() {
+    var birthYear = prompt("What year were you born?")
+    var currentYear = new Date().getFullYear()
+    var ageInDays = (currentYear - birthYear) * 365
+
+    var h1 = document.createElement("h1")
+    var answer = document.createTextNode("You are " + ageInDays + " days old.")
+
+    h1.setAttribute("id", "ageInDays")
+    h1.appendChild(answer)
+
+    document.getElementById("flexbox-result").appendChild(h1)
+}
